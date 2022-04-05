87e826523a6502a013f18cd1f7670cb521306ce5
Ericsson715 <92638586+Ericsson715@users.noreply.github.com>
2022-04-05 15:12:34 -0500

    Commit inicial del chat

---
+++ app/src/main/java/com/example/acercade/ui/home/PreciosFragment.java
@@ -0,0 +1,38 @@
+package com.example.acercade.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+
+import com.example.acercade.databinding.FragmentPreciosBinding;
+
+public class PreciosFragment extends Fragment {
+
+    private FragmentPreciosBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentPreciosBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/com/example/acercade/ui/notifications/ExperienciaFragment.java
@@ -0,0 +1,37 @@
+package com.example.acercade.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.acercade.databinding.FragmentPreciosBinding;
+
+
+public class ExperienciaFragment extends Fragment {
+
+    private FragmentPreciosBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        NotificationsViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(NotificationsViewModel.class);
+
+        binding =  FragmentPreciosBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
